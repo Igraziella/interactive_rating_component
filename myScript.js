a8680d025b51f1ae556cde5cf998a414e8aa848f
igraziella <igraziella2@gmail.com>
2023-12-08 19:49:00 +0100

    ft: added javascript

---
+++ myScript.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container")
+const thankscontainer = document.querySelector(".container2")
+const submit = document.getElementById("submit")
+const rating = document.getElementById("rating")
+const rates = document.querySelectorAll(".btn-num")
+
+submit.addEventListener("click", () => {
+    thankscontainer.classList.remove("hidden")
+    container.style.display = "none"
+})
+
+rates.forEach((rate) => {
+    rate.addEventListener("click", () => {
+        rating.innerHTML = rate.innerHTML
+        console.log(ratting.innerHTML);
+    })
+})
